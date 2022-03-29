f3d4a847b2d5c76428b3faad746e646013a27822
aviation4 <przemek.kaminski4@gmail.com>
2022-03-29 14:23:50 +0200

    Temporary source files in main folder

---
+++ js/InputHandler.js
@@ -0,0 +1,187 @@
+import {grid, gameBoard, setupInput, touches, endGameBoard, hiddenClass, maxtile, MAX_TILE} from "./main2.js";
+import Grid from "./Grid.js";
+import Tile from "./Tile.js";
+import {endGame, endGameScoreUpdate} from "./EndgameHandler.js";
+import {SCORE} from "./Cell.js";
+
+
+
+export const determineTouchDirection = () => {
+
+  /* Distance in X direction */
+  const distanceX = touches[0][1] - touches [0][0];
+
+  /* Distance in Y direction */
+  const distanceY = touches[1][1] - touches [1][0];
+
+
+  /* if Y distance is bigger than X distance, then vertical direction */
+  if (Math.abs(distanceX) > Math.abs(distanceY)){
+
+    if (distanceX > 0) return "ArrowRight";
+    else return "ArrowLeft";
+
+  } else {
+
+    if (distanceY > 0) return "ArrowDown";
+    else return "ArrowUp";
+
+  }
+
+}
+
+
+export const handlerInput = async e => {
+
+  let direction;
+
+  if (typeof e == "object"){
+    direction = e.key;
+  } else if (typeof e == "string"){
+    direction = e;
+  }
+
+  switch (direction){
+    case "ArrowUp":
+    if (!canMoveUp()){
+      setupInput();
+      return;
+    }
+      await moveUp();
+      break;
+
+
+    case "ArrowDown":
+    if (!canMoveDown()){
+      setupInput();
+      return;
+    }
+      await moveDown();
+      break;
+
+
+    case "ArrowLeft":
+    if (!canMoveLeft()){
+      setupInput();
+      return;
+    }
+      await moveLeft();
+      break;
+
+
+    case "ArrowRight":
+    if (!canMoveRight()){
+      setupInput();
+      return;
+    }
+      await moveRight();
+      break;
+
+
+    default:
+      setupInput();
+      return;
+  }
+
+
+  grid.cells.forEach(cell => cell.mergeTiles());
+
+  if (!(endGameBoard.classList.contains(hiddenClass))){
+    endGameScoreUpdate();
+    return;
+  }
+
+  const newTile = new Tile(gameBoard);
+  MAX_TILE = Math.max(MAX_TILE, newTile.value);
+  maxtile.innerHTML = `Max tile: ${MAX_TILE}`;
+  grid.randomEmptyCell().tile = newTile;
+
+  touches[0].length = 0;
+  touches[1].length = 1;
+
+
+  if(!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()){
+    newTile.waitForTransition(true).then(() => {
+      endGame(false);
+    })
+  } else {
+    setupInput();
+  }
+}
+
+const moveUp = () => {
+  slideTiles(grid.cellsByColumn);
+}
+
+const moveLeft = () => {
+  slideTiles(grid.cellsByRow);
+}
+
+const moveDown = () => {
+  slideTiles(grid.cellsByColumn.map(column => [...column].reverse()));
+}
+
+const moveRight = () => {
+  slideTiles(grid.cellsByRow.map(row => [...row].reverse()));
+}
+
+
+
+
+
+const slideTiles = (cells) => {
+  return Promise.all(
+
+  cells.flatMap(group => {
+    const promises = [];
+    for (let i = 1; i < group.length; i++) {
+      const cell = group[i];
+      if (cell.tile == null) continue;
+      let lastValidCell;
+      for (let j = i - 1; j >= 0; j--){
+        const moveToCell = group[j];
+        if (!moveToCell.canAccept(cell.tile)) break;
+        lastValidCell = moveToCell;
+      }
+
+      if (lastValidCell != null){
+        promises.push(cell.tile.waitForTransition());
+        if (lastValidCell.tile != null){
+          lastValidCell.mergeTile = cell.tile;
+        } else {
+          lastValidCell.tile = cell.tile;
+        }
+        cell.tile = null;
+      }
+    }
+    return promises;
+  }))
+}
+
+
+const canMoveUp = () => {
+  return canMove(grid.cellsByColumn);
+}
+
+const canMoveDown = () => {
+  return canMove(grid.cellsByColumn.map(column => [...column].reverse()));
+}
+
+const canMoveLeft = () => {
+  return canMove(grid.cellsByRow);
+}
+
+const canMoveRight = () => {
+  return canMove(grid.cellsByRow.map(row => [...row].reverse()));
+}
+
+const canMove = cells => {
+  return cells.some(group => {
+    return group.some((cell, index) => {
+      if(index == 0) return false;
+      if (cell.tile == null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    })
+  })
+}
---
+++ js/Tile.js
@@ -0,0 +1,77 @@
+const tileColors = ['#a7d0cd', '#b8c0b8', '#bfb2a7', '#c1a49a', '#bf988f', '#b98c86', '#b18180', '#a7787c', '#9a6f7a', '#8c6779', '#7b6079']
+import {tile, gridSize, baseNumber} from "./main2.js";
+
+export default class Tile {
+  #tileElement
+  #x
+  #y
+  #value
+
+  constructor (tileContainer, value = Math.random() > 0.5 ? baseNumber : baseNumber * 2){
+    this.#tileElement = document.createElement("div");
+    this.#tileElement.classList.add("tile");
+    this.#tileElement.setAttribute("id", "tile");
+    tileContainer.append(this.#tileElement);
+    this.value = value;
+  }
+
+
+  set x (position){
+    this.#x = position
+    this.#tileElement.style.setProperty("--x", position);
+  }
+
+
+  set y (position){
+    this.#y = position
+    this.#tileElement.style.setProperty("--y", position);
+  }
+
+  get value (){
+    return this.#value;
+  }
+
+
+  set value (v) {
+    this.#value = v;
+    this.#tileElement.textContent = v;
+    const power = Math.log2(v);
+    const index = Math.floor(power);
+    this.#tileElement.style.setProperty(
+      "background-color",
+      tileColors[index-1]
+    );
+    if (index >= 8){
+      this.#tileElement.style.setProperty(
+       "color",
+        "#F5F5F5"
+      );
+    }
+    if (gridSize == 3){
+      this.#tileElement.style.setProperty("border-radius", "8vmin");
+      this.#tileElement.style.setProperty("font-size", "6vmin");
+    } else if (gridSize == 4){
+      this.#tileElement.style.setProperty("border-radius", "6vmin");
+      this.#tileElement.style.setProperty("font-size", "5vmin");
+    } else if (gridSize == 5){
+      this.#tileElement.style.setProperty("border-radius", "5vmin");
+      this.#tileElement.style.setProperty("font-size", "4vmin");
+    }
+
+  }
+
+  remove() {
+    this.#tileElement.remove();
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise(resolve => {
+      this.#tileElement.addEventListener(
+        animation ? "animationend" : "transitionend",
+        resolve, {
+        once: true,
+      })
+    })
+  }
+
+}
